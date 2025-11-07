6bb93e2ab88af2922d5cc5ff7a0b545871a540b1
naveenmoka <naveenmoka2004@gmail.com>
2025-11-07 22:31:52 +0530

    Creating Button List && Getting youtube data

--- src/Components/ButtonList.js
+++ src/Components/ButtonList.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Button from "./Button";
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  const list = [
+    "All",
+    "Live",
+    "Gaming",
+    "Songs",
+    "Live",
+    "Cinema",
+    "Telugu Cinema",
+    "Sports",
+    "Cricket",
+    "Recently upload",
+  ];
+  return (
+    <div className="flex">
+      {list.map((text, index) => (
+        <Button key={index} name={text} />
+      ))}
+    </div>
+  );
 };
 
 export default ButtonList;
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyB_7LgiRfU-Ix-mK-USwVA1U8q-0F56vA8";
+
+export const YOUTUBE_VIDEO_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY;
